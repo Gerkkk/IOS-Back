6f251a9000e3420899244362f2841972b0f16636
Gerkkk <danpol2005@gmail.com>
2025-01-25 20:50:09 +0300

    simple handler

--- handler.go
+++ handler.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	userActions := router.Group("/user-actions")
+	{
+		userActions.GET("/fetch-people", h.fetchPeople)
+	}
+
+	return router
+}
--- main.go
+++ main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
-	gotype "github.com/Gadzet005/GoType/backend"
+	"github.com/Gerkkk/IOS-Back/main"
 )
 
-// @title GoType App API
-// @version 0.0.1
-// @description API Server for GoType game and website
-
-// @host localhost:8000
-// @BasePath/
-
-// @securityDefinitions.apikey ApiKeyAuth
-// @in header
-// @name Authorization
-
 func main() {
-	srv := new(main.Server)
-	if err := srv.Run(8000, handlers.InitRoutes()); err != nil {
+	srv := new(Server)
+	if err := srv.Run("8000", InitRoutes()); err != nil {
 		fmt.Println("failed to run server")
 	}
 }
